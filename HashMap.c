c00eb00b8c83bce43a5c6e08be2f0b7386665d54
bryank458 <bryank458>
2019-01-23 18:24:53 -0800

    initial

---
+++ HashMap.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct HashNode HashNode;
+typedef struct HashMap HashMap;
+
+struct HashNode {
+   char *key;
+   int value;
+   HashNode *next;
+};
+
+struct HashMap {
+   int numItems;
+   int fullSize;
+   HashNode **array;
+};
+
+HashMap *initHashMap(int fullSize);
+HashMap *rehash(HashMap *dictionary);
+int hash(HashMap *dictionary, char *str);
+HashMap *insert(HashMap *dictionary, char *key, int val);
+void printHashMap(HashMap *dictionary);
+void freeHashMap(HashMap *map);
+void freeHashArray(HashNode **array, int numOfItems);
+
+HashMap *initHashMap(int fullSize) {
+   HashMap *dictionary;
+   dictionary = (HashMap *)malloc(sizeof(HashMap));
+
+   if(!dictionary) {
+      perror("initHashMap");
+      exit(EXIT_FAILURE);
+   }
+
+   dictionary->numItems = 0;
+   dictionary->fullSize = fullSize;
+
+   dictionary->array = (HashNode**)calloc(fullSize, sizeof(HashNode*));
+
+   if(!dictionary->array) {
+      perror("initHashMap");
+      exit(EXIT_FAILURE);
+   }
+
+
+   return dictionary;
+}
+
+HashMap *rehash(HashMap *dictionary) {
+   HashMap *newMap;
+   int i;
+
+   newMap = initHashMap((dictionary->fullSize) ^ 2);
+
+   for(i=0; i<(dictionary->fullSize); i++) {
+      if (dictionary->array[i] != NULL) {
+         insert(newMap, dictionary->array[i]->key, dictionary->array[i]->value);
+      }
+   }
+
+   freeHashMap(dictionary);
+
+   return newMap;
+}
+
+int hash(HashMap *dictionary, char *str) {
+   int hash = 5381;
+   int c;
+
+   while ((c = *str++))
+      hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+   if (hash < 0) {
+      return - hash % (dictionary->fullSize);
+   }
+   return hash % (dictionary->fullSize);
+}
+
+HashMap *insert(HashMap *dictionary, char *key, int val){
+   char *cpyKey;
+   double loadFactor;
+   int pos;
+   HashNode *temp;
+   HashNode *newNode;
+   HashNode *list;
+
+   cpyKey = (char *)malloc(sizeof(char)*strlen(key));
+   strcpy(cpyKey, key);
+
+   loadFactor = (double)(dictionary->numItems)/(double)(dictionary->fullSize);
+   if (loadFactor >= 0.5) {
+      dictionary = rehash(dictionary);
+   }
+
+   pos = hash(dictionary, cpyKey);
+   list = dictionary->array[pos];
+   newNode = (HashNode *)malloc(sizeof(HashNode));
+
+   if(!newNode) {
+      perror("insert");
+      exit(EXIT_FAILURE);
+   }
+
+   temp = list;
+   while(temp != NULL){
+      if(strcmp(temp->key, cpyKey) == 0){
+         temp->value = temp->value + 1;
+         free(cpyKey);
+         free(newNode);
+         return dictionary;
+      }
+      temp = temp->next;
+   }
+   dictionary->numItems += 1;
+   newNode->key = cpyKey;
+   newNode->value = val;
+   newNode->next = list;
+   dictionary->array[pos] = newNode;
+   return dictionary;
+}
+
+void printHashMap(HashMap *dictionary) {
+   int i;
+   printf("{ ");
+   for(i=0; i<(dictionary->fullSize); i++) {
+      if (dictionary->array[i] != NULL) {
+         printf("[\"%s\", %d], ", dictionary->array[i]->key,
+                dictionary->array[i]->value);
+      }
+   }
+   printf("\b\b}\n");
+
+}
+
+HashNode **hashmapToArray(HashMap *dictionary) {
+
+   HashNode **nodeMap = (HashNode**)calloc((dictionary->numItems),
+                         sizeof(HashNode*));
+
+   int i;
+   int arrayCounter;
+   arrayCounter = 0;
+
+   for(i=0; i<(dictionary->fullSize); i++) {
+      if (dictionary->array[i] != NULL) {
+         nodeMap[arrayCounter] = dictionary->array[i];
+         arrayCounter++;
+      }
+   }
+   return nodeMap;
+}
+
+void freeHashMap(HashMap *dictionary) {
+
+   int i;
+   for(i=0; i<(dictionary->fullSize); i++) {
+      if (dictionary->array[i] != NULL) {
+         free(dictionary->array[i]->key);
+         free(dictionary->array[i]);
+      }
+   }
+   free(dictionary->array);
+   free(dictionary);
+   return;
+
+}
+
+void freeHashArray(HashNode **array, int numOfItems) {
+
+   free(array);
+}
---
+++ fw.c
@@ -0,0 +1,135 @@
+/*
+ *Name: Bryan Kwong
+ *Instructor: P. Nico
+ *Section: CPE357-05
+ *Version: Asgn 2 - fw
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "HashMap.c"
+
+HashMap *addFileToMap(FILE *fileToProcess, HashMap *map);
+
+int main(int argc, char **argv) {
+
+   int numOfWords;
+   HashMap *wordMap;
+   HashNode **nodeMap;
+   int argCounter;
+   int i, j;
+
+   wordMap = initHashMap(1000);
+   numOfWords = 10;
+   argCounter = 0;
+
+   while(argCounter < (argc-1)) {
+      argCounter += 1;
+      if(strcmp(argv[argCounter], "-n") == 0) {
+         if ((argCounter += 1) > (argc-1)) {
+            printf("usage: fw [-n num] [ file1 [ file2 [...] ] ]\n");
+            exit(EXIT_FAILURE);
+         }
+         numOfWords = atoi(argv[argCounter]);
+         if (numOfWords == 0) {
+            printf("%s: not a well-formed number\n", argv[argCounter]);
+            printf("usage: fw [-n num] [ file1 [ file2 [...] ] ]\n");
+            exit(EXIT_FAILURE);
+         }
+      } else {
+
+         FILE *fileToProcess;
+         fileToProcess = fopen(argv[argCounter], "r");
+
+         if(!fileToProcess) {
+            perror(argv[argCounter]);
+            // exit(EXIT_FAILURE);
+         } else {
+            wordMap = addFileToMap(fileToProcess, wordMap);
+            fclose(fileToProcess);
+         }
+
+      }
+   }
+
+   printf("The top %d words (out of %d) are:\n", numOfWords, wordMap->numItems);
+   nodeMap = hashmapToArray(wordMap);
+   for (i=0; i<numOfWords; i++) {
+      HashNode *maxNode;
+      maxNode = NULL;
+      maxNode = nodeMap[0];
+      for (j=1; j<(wordMap->numItems); j++) {
+         if (nodeMap[j]->value > maxNode->value) {
+            maxNode = nodeMap[j];
+         } else if (nodeMap[j]->value == maxNode->value) {
+            if (strcmp(nodeMap[j]->key, maxNode->key) > 0) {
+               maxNode = nodeMap[j];
+            }
+         }
+      }
+      if (maxNode != NULL && maxNode->value != 0) {
+         printf("%9d %s\n", maxNode->value, maxNode->key);
+         maxNode->value = 0;
+      }
+
+      if(i > wordMap->numItems)
+         break;
+   }
+   freeHashMap(wordMap);
+   free(nodeMap);
+   return 0;
+}
+
+HashMap *addFileToMap(FILE *fileToProcess, HashMap *map) {
+
+   int newChar;
+
+   int wordLocation;
+   int wordLength;
+   char *currentWord;
+
+   wordLocation = 0;
+   wordLength = 1000;
+   currentWord = (char *)calloc(wordLength, sizeof(char));
+
+   if(!currentWord) {
+      perror("addToHashMap");
+      printf("The top 10 words (out of 0) are:\n" );
+      exit(EXIT_FAILURE);
+   }
+
+   while ((newChar = fgetc(fileToProcess)) != EOF) {
+      newChar = tolower(newChar);
+      if (wordLocation >= (wordLength - 1)) {
+         currentWord = realloc(currentWord, wordLength *= 2);
+         if(!currentWord) {
+            perror("addToHashMap");
+            exit(EXIT_FAILURE);
+         }
+      }
+      if (isalpha(newChar)) {
+         currentWord[wordLocation++] = newChar;
+      } else if (strlen(currentWord) != 0) {
+         map = insert(map, currentWord, 1);
+         free(currentWord);
+
+         wordLength = 100;
+         wordLocation = 0;
+         currentWord = (char *)calloc(wordLength, sizeof(char));
+      }
+   }
+
+   if(strlen(currentWord) != 0) {
+      map = insert(map, currentWord, 1);
+      free(currentWord);
+
+      wordLength = 100;
+      wordLocation = 0;
+      currentWord = (char *)calloc(wordLength, sizeof(char));
+   }
+
+   return map;
+
+}
