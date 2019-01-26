85e6002ed56f5cf2affc2519348d088da6c4f5b5
bryank458 <bryank458>
2019-01-25 19:35:55 -0800

    FINISHED

--- HashMap.c
+++ HashMap.c
@@ -1,3 +1,10 @@
+/*
+ *Name: Bryan Kwong
+ *Instructor: P. Nico
+ *Section: CPE357-05
+ *Version: Asgn 2 - fw/HashMap.c
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -25,6 +32,11 @@ void freeHashMap(HashMap *map);
 void freeHashArray(HashNode **array, int numOfItems);
 
 HashMap *initHashMap(int fullSize) {
+/* Function to create a hash map
+ * PARAMS: int - The initial size of the hashmap array
+ * RETURN: HashMap* - A Hashmap
+ */
+
    HashMap *dictionary;
    dictionary = (HashMap *)malloc(sizeof(HashMap));
 
@@ -36,6 +48,7 @@ HashMap *initHashMap(int fullSize) {
    dictionary->numItems = 0;
    dictionary->fullSize = fullSize;
 
+   /* create the hash array of all 0*/
    dictionary->array = (HashNode**)calloc(fullSize, sizeof(HashNode*));
 
    if(!dictionary->array) {
@@ -48,23 +61,39 @@ HashMap *initHashMap(int fullSize) {
 }
 
 HashMap *rehash(HashMap *dict) {
+/* When the load factor of the hash table reaches 0.50, rehash is 
+ * called to increase the size of the hashmap
+ * PARAMS: HashMap* - A *Hashmap that will be rehashed
+ * RETURNS: HashMap* - A *Hashmap that has been rehashed by a power of 2
+ */
+
    HashMap *newMap;
    int i;
 
+   /* create a new hashmap that will be have a sized increased by a 
+      power of 2*/
    newMap = initHashMap((dict->fullSize) << 2);
 
+   /* Loop through the old hashmap and reinsert them into the new one*/
    for(i=0; i<(dict->fullSize); i++) {
       if (dict->array[i] != NULL) {
          insert(newMap, dict->array[i]->key, dict->array[i]->value);
       }
    }
 
+   /*free the old hashmap*/
    freeHashMap(dict);
 
    return newMap;
 }
 
 int hash(HashMap *dictionary, char *str) {
+/* The hash function that turns a string into a location on a hashmap
+ * PARAMS: char* - the string to be hashed 
+ *         HashMap* - the *Hashmap that the string will be hashed into
+ * RETURN: int - the hash value aka location on the array in the hashmap
+ */
+
    int hash = 5381;
    int c;
 
@@ -77,10 +106,39 @@ int hash(HashMap *dictionary, char *str) {
    return hash % (dictionary->fullSize);
 }
 
+int Althash(HashMap *dictionary, char *str) {
+/* Alternative Hashing scheme created during development 
+ * PARAMS: char* - the string to be hashed 
+ *         HashMap* - the *Hashmap that the string will be hashed into
+ * RETURN: int - the hash value aka location on the array in the hashmp
+ */
+
+   int hash = 0;
+   int c;
+
+   while ((c = *str++))
+      hash = c + (hash << 6) + (hash << 16) - hash;
+
+   if (hash < 0) {
+      return - hash % (dictionary->fullSize);
+   }
+   return hash % (dictionary->fullSize);
+}
+
 HashMap *insert(HashMap *dictionary, char *key, int val){
+/* Inserts a Hashnode consisting of the key value pair into the Hashmap.
+ * If the key already exist inside the hashmap, the value of that hashnode
+ * will be incremented by 1.
+ * PARAMS: HashMap* - The *Hashmap to be inserted into 
+ *         char* - The string that will act as the key 
+ *         int - the number of occurances that a word has existed
+ * RETURN: HashMap* - The hashmap with the key value pair inserted
+ */
+
    char *cpyKey;
    double loadFactor;
    int pos;
+
    HashNode *currentLocation;
    HashNode *newNode;
    HashNode *list;
@@ -88,9 +146,9 @@ HashMap *insert(HashMap *dictionary, char *key, int val){
    cpyKey = (char *)malloc(sizeof(char)*(strlen(key)+1));
    strcpy(cpyKey, key);
 
+   /* If load factor is greater than 0.5, rehash and increase size */
    loadFactor = (double)(dictionary->numItems)/(double)(dictionary->fullSize);
    if (loadFactor >= 0.5) {
-      printHashMap(dictionary);
       dictionary = rehash(dictionary);
    }
 
@@ -98,25 +156,33 @@ HashMap *insert(HashMap *dictionary, char *key, int val){
    list = dictionary->array[pos];
    newNode = (HashNode *)malloc(sizeof(HashNode));
 
-
    if(!newNode) {
       perror("insert");
       exit(EXIT_FAILURE);
    }
 
+   /* If the hashed location is already filled, do linear probing until 
+      a spot is open */
    currentLocation = list;
-   while(currentLocation != NULL){
+   while(currentLocation != 0){
+      /* Compare to see if current node has the same key, it is has already
+         occured, just increment 1 to the value */
       if(strcmp(currentLocation->key, cpyKey) == 0){
          currentLocation->value = currentLocation->value + 1;
          free(cpyKey);
          free(newNode);
          return dictionary;
       }
-      currentLocation = (pos < (dictionary->fullSize))?
+      /*Linear probing to go to the next, if end of array is reached, go 
+        back to the front */
+      currentLocation = (pos < (dictionary->fullSize-1))?
                          dictionary->array[pos+=1]:
-                         dictionary->array[0];
+                         dictionary->array[(pos = 0)];
+
    }
 
+   /* If the string in question has never occured before, add the new node to
+      to the hashed location*/
    dictionary->numItems += 1;
    newNode->key = cpyKey;
    newNode->value = val;
@@ -126,10 +192,16 @@ HashMap *insert(HashMap *dictionary, char *key, int val){
 }
 
 void printHashMap(HashMap *dictionary) {
+/* Loop through the Hashmap and print all of the nodes as 
+ * key value pairs 
+ * PARAMS: HashMap* - hashmap to be printed
+ * RETURN: void
+ */
+
    int i;
    printf("{ ");
    for(i=0; i<(dictionary->fullSize); i++) {
-      if (dictionary->array[i] != NULL) {
+      if (dictionary->array[i] != 0) {
          printf("[\"%s\", %d], ", dictionary->array[i]->key,
                 dictionary->array[i]->value);
       }
@@ -139,7 +211,12 @@ void printHashMap(HashMap *dictionary) {
 }
 
 HashNode **hashmapToArray(HashMap *dictionary) {
-
+/* Loop through the hashmap to find all the nodes, and then put all these 
+ * nodes into an array where it is to be referred to later
+ * PARAMS: HashaMap * - The HashMap to be turned into an array 
+ * RETURNS: HashNode ** - An array of HashNodes with no NULL
+ */
+ 
    HashNode **nodeMap = (HashNode**)calloc((dictionary->numItems),
                          sizeof(HashNode*));
 
@@ -148,7 +225,7 @@ HashNode **hashmapToArray(HashMap *dictionary) {
    arrayCounter = 0;
 
    for(i=0; i<(dictionary->fullSize)+1; i++) {
-      if (dictionary->array[i] != NULL) {
+      if (dictionary->array[i] != 0) {
          nodeMap[arrayCounter] = dictionary->array[i];
          arrayCounter += 1;
       }
@@ -157,10 +234,14 @@ HashNode **hashmapToArray(HashMap *dictionary) {
 }
 
 void freeHashMap(HashMap *dictionary) {
+/* Free the allocated Hashmap
+ * PARAMS: HashMap* - The *Hashmap to be freed
+ * RETURN: void
+ */
 
    int i;
    for(i=0; i<(dictionary->fullSize); i++) {
-      if (dictionary->array[i] != NULL) {
+      if (dictionary->array[i] != 0) {
          free(dictionary->array[i]->key);
          free(dictionary->array[i]);
       }
@@ -170,9 +251,3 @@ void freeHashMap(HashMap *dictionary) {
    return;
 
 }
-
-void freeHashArray(HashNode **array, int numOfItems) {
-
-   free(array);
-}
-
--- fw.c
+++ fw.c
@@ -2,7 +2,7 @@
  *Name: Bryan Kwong
  *Instructor: P. Nico
  *Section: CPE357-05
- *Version: Asgn 2 - fw
+ *Version: Asgn 2 - fw/fw.c
  */
 
 #include <stdio.h>
@@ -20,51 +20,70 @@ int main(int argc, char **argv) {
    HashNode **nodeMap;
    int argCounter;
    int i, j, cnt;
+   char *strtolPtr;
 
-   wordMap = initHashMap(2000000);
+   /* Initalize a hashmap to store words */
+   wordMap = initHashMap(10000);
+   /*Initialize the default number of words to print*/
    numOfWords = 10;
+   /*Keeping a separate argument counter to prevent corrupting original one*/
    argCounter = 0;
+   /*Keep count of how many files were processed*/
    cnt = 0;
 
+   /* Loop through terminal arguments */
    while(argCounter < (argc-1)) {
       argCounter += 1;
+
       if(strcmp(argv[argCounter], "-n") == 0) {
+         /*If -n is encountered, go to the next arg to check for number*/
          if ((argCounter += 1) > (argc-1)) {
             printf("usage: fw [-n num] [ file1 [ file2 [...] ] ]\n");
             exit(EXIT_FAILURE);
          }
          numOfWords = atoi(argv[argCounter]);
-         if (numOfWords == 0) {
+         /*If atoi returns zero, check to see if it is an error or a int 0*/
+         if (numOfWords == 0 && argv[argCounter][0] != 48) {
             printf("%s: not a well-formed number\n", argv[argCounter]);
             printf("usage: fw [-n num] [ file1 [ file2 [...] ] ]\n");
             exit(EXIT_FAILURE);
          }
       } else {
-
+         /*open file to see if the file exists, increment file count*/
          FILE *fileToProcess;
          fileToProcess = fopen(argv[argCounter], "r");
          cnt += 1;
 
          if(!fileToProcess) {
             perror(argv[argCounter]);
-            // exit(EXIT_FAILURE);
          } else {
+            /*add all the words in the file to the hashmap*/
             wordMap = addFileToMap(fileToProcess, wordMap);
             fclose(fileToProcess);
          }
       }
    }
 
+   /*If no files were given aka file count (cnt) is 0, check stdin*/
    if (cnt == 0) {
       wordMap = addFileToMap(stdin, wordMap);
    }
 
+   /*prepare outputs to stdout*/
    printf("The top %d words (out of %d) are:\n", numOfWords, wordMap->numItems);
+   /*turn the hashmap into an array so no time would be wasted looping through
+     NULL*/
    nodeMap = hashmapToArray(wordMap);
+
+   /*Loop through the array to find the n most frequent words */
    for (i=0; i<numOfWords; i++) {
+
+      /*establish a hashnode pointer that points to the current max*/
       HashNode *maxNode;
       maxNode = NULL;
       maxNode = nodeMap[0];
+      /*loop through array to see if a higher value is found, if it is found 
+        replace the maximum node with this new found higher valued node */ 
       for (j=1; j<(wordMap->numItems); j++) {
          if (nodeMap[j]->value > maxNode->value) {
             maxNode = nodeMap[j];
@@ -74,14 +93,19 @@ int main(int argc, char **argv) {
             }
          }
       }
+      /* if a new Maxnode is found, set the maxnode value to 0 and print
+         its contents. Setting to 0 ensures it will not be displayed again*/
       if (maxNode != NULL && maxNode->value != 0) {
          printf("%9d %s\n", maxNode->value, maxNode->key);
          maxNode->value = 0;
       }
 
+      /*If no more words exists break the loop*/
       if(i > wordMap->numItems)
          break;
    }
+   
+   /*free the hashmap and the allocated array*/
    freeHashMap(wordMap);
    free(nodeMap);
    return 0;
